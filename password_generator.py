03c4b4e604d832da09f626ee51c1ce08a82157d4
Vlad <vladh17dmv@mail.ru>
2022-07-25 19:04:59 +0300

    init: init project

---
+++ password_generator.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import random
+import sqlite3
+
+
+class PasswordGenerator():
+    password = ''
+    length = 10
+    include_symbols = True
+    include_capital_letters = True
+    include_numbers = True
+    symbols = """~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/"""
+    numbers = '0123456789'
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    
+    def generate_password(self):
+        pass
+
+
+    def save_password(self):
+        pass
+
+
+    def show_saved_passwords(slef):
+        pass
+
+
+pw = PasswordGenerator()
+
+
+
